1ee61dfa01c880ce460af7c695fc4369105ad677
Joachim Vandekerckhove <Joachim Vandekerckhove>
2025-01-29 15:52:16 -0800

    Clean up ttd

--- src/tdd/BayesFactor.py
+++ src/tdd/BayesFactor.py
@@ -1,6 +1,4 @@
-import scipy.integrate as spi
-import numpy as np
-from scipy.special import comb
+import scipy
 
 class BayesFactor:
     def __init__(self, n, k, prior1, prior2):
@@ -10,7 +8,7 @@ class BayesFactor:
         self.prior2 = prior2
     
     def binomial_pdf(self, p, n, k):
-        return comb(n, k, exact=False) * (p ** k) * ((1 - p) ** (n - k))
+        return scipy.special.comb(n, k, exact=False) * (p ** k) * ((1 - p) ** (n - k))
     
     def integrand1(self, p):
         return self.binomial_pdf(p, self.n, self.k) * self.prior1(p)
@@ -19,8 +17,8 @@ class BayesFactor:
         return self.binomial_pdf(p, self.n, self.k) * self.prior2(p)
 
     def compute(self):
-        integration1 = spi.quad(self.integrand1, 0, 1)[0]
-        integration2 = spi.quad(self.integrand2, 0, 1)[0]
+        integration1 = scipy.integrate.quad(self.integrand1, 0, 1)[0]
+        integration2 = scipy.integrate.quad(self.integrand2, 0, 1)[0]
         return integration2 / integration1
 
 def uniform_prior1(p):
